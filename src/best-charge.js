eb72c6742ef6c5d3914edfa3d95304147da486de
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-05 20:38:48 +0800

    Parse selectedItems to quantity map

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,13 @@
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  var countMap = selectedItems.map(item => {
+    return {
+      name: item.split('x')[0].trim(),
+      quantity: item.split('x')[1].trim()
+    }
+  });
+  console.log(countMap);
+
+  return "Not Implemented";
 }
+
+module.exports = bestCharge;
