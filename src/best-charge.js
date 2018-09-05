79e1024de359842ae1105c264d375c3542bcba06
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-05 21:25:31 +0800

    Added calculate price for order

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,13 +1,31 @@
+const loadPromotions = require('./promotions');
+const loadAllItems = require('./items')
+
 function bestCharge(selectedItems) {
-  var countMap = selectedItems.map(item => {
+  let order = selectedItems.map(str => {
+    let id = str.split('x')[0].trim();
+    let quantity = parseInt(str.split('x')[1].trim());
+    let item = findItemById(id);
     return {
-      name: item.split('x')[0].trim(),
-      quantity: item.split('x')[1].trim()
+      id: id,
+      item: item,
+      quantity: quantity,
+      subtotal: item.price * quantity
     }
-  });
-  console.log(countMap);
+  }).reduce((order, item) => {
+    order.items.push(item);
+    order.price += item.subtotal;
+    order.totalPrice += item.subtotal;
+    return order;
+  }, {items: [], price: 0, discount: 0, totalPrice: 0});
+
+  console.log(order);
 
   return "Not Implemented";
+}
+
+function findItemById(id) {
+  return loadAllItems().find(item => item.id === id);
 }
 
 module.exports = bestCharge;
