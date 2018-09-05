4b424e07e53593456fd4150d98f36e1f1fa864b9
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-05 22:52:10 +0800

    Finished return invoice

--- src/best-charge.js
+++ src/best-charge.js
@@ -26,13 +26,22 @@ function bestCharge(selectedItems) {
     }
   }).sort(((a, b) => b.discount - a.discount));
 
-  order.promotion = discounts[0];
+  order.promotion = discounts[0].promotion;
   order.discount = discounts[0] ? discounts[0].discount : 0;
   order.totalPrice = order.price - order.discount;
 
-  console.log(order);
-
-  return "Not Implemented";
+  //printOrder
+  let invoice = '============= 订餐明细 =============\n';
+  invoice += order.items.map(item => `${item.item.name} x ${item.quantity} = ${item.subtotal}元\n`).join("")
+  invoice += '-----------------------------------\n';
+  if (order.discount !== 0) {
+    invoice += '使用优惠:\n';
+    invoice += `${order.promotion.getName()}，省${order.discount}元\n`;
+    invoice += '-----------------------------------\n';
+  }
+  invoice += `总计：${order.totalPrice}元\n`;
+  invoice += '===================================';
+  return invoice;
 }
 
 function findItemById(id) {
--- src/promotions.js
+++ src/promotions.js
@@ -1,16 +1,26 @@
+const loadAllItems = require('./items');
+
 function loadPromotions() {
   return [{
     type: '满30减6元',
-    calculate: priceBreakDiscount(30, 6)
+    calculate: priceBreakDiscount(30, 6),
+    getName: function () {
+      return this.type;
+    }
   }, {
     type: '指定菜品半价',
     items: ['ITEM0001', 'ITEM0022'],
-    calculate: halfPriceDiscount(['ITEM0001', "ITEM0022"])
+    calculate: halfPriceDiscount,
+    getName: function () {
+      let involvedItemsName = this.items.map(id => loadAllItems().find(item => item.id === id))
+        .map(item => item.name);
+      return `${this.type}(${involvedItemsName.join("，")})`
+    }
   }];
 }
 
 function priceBreakDiscount(upTo, discount) {
-  return function(order){
+  return function (order) {
     if (order.price >= upTo) {
       return discount;
     }
@@ -18,11 +28,9 @@ function priceBreakDiscount(upTo, discount) {
   }
 }
 
-function halfPriceDiscount(items) {
-  return function (order) {
-    return order.items.filter(item => items.includes(item.id))
-      .reduce((totalPrice, item) => totalPrice + item.subtotal, 0) / 2;
-  };
+function halfPriceDiscount(order) {
+  return order.items.filter(item => this.items.includes(item.id))
+    .reduce((totalPrice, item) => totalPrice + item.subtotal, 0) / 2;
 }
 
 module.exports = loadPromotions;
